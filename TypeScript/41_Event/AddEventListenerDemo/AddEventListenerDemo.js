10d8a64bdd6158c00e68efadd6d69fbaba5acc2b
RedPlus <redplus@redplus.net>
2020-07-10 18:30:07 +0900

    41_01_Event_AddEventListenerDemo_addEventListener 함수를 사용하여 버튼에 click 이벤트 적용하기

---
+++ TypeScript/41_Event/AddEventListenerDemo/AddEventListenerDemo.js
@@ -0,0 +1,6 @@
+var btn = document.getElementById("btn");
+// 이벤트 이름과 무명 메서드를 매개 변수로 지정
+btn.addEventListener("click", function () {
+    console.log("Clicked!!!");
+});
+//# sourceMappingURL=AddEventListenerDemo.js.map
