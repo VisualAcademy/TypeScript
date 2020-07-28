98cb89b22019aef531501be9d4ff0caa7084d4e9
RedPlus <redplus@redplus.net>
2020-07-28 20:48:35 +0900

    43_10_AbstractClass_추상 클래스와 추상 메서드

--- TypeScript/43_Inheritance/10_AbstractClass/AbstractClass.ts
+++ TypeScript/43_Inheritance/10_AbstractClass/AbstractClass.ts
@@ -1,8 +1,14 @@
-// 추상 클래스
+//[?] 추상 클래스(Abstract Class):
+//    기본(부모) 클래스 역할을 하여 파생(자식) 클래스에게 
+//    특정 멤버를 반드시 구현하도록 멤버 리스트를 제공
+
+//[1] 추상 클래스
 abstract class Shape {
+    //[2] 추상 멤버: 추상 메서드
     abstract getArea(): number; 
 }
 
+//[3] 추상 클래스를 상속하는 클래스
 // 원
 class Circle extends Shape {
     constructor(public radius: number) {
@@ -28,6 +34,7 @@ class Square extends Shape {
     constructor(public size: number) {
         super();
     }
+    //[4] 부모 클래스인 Shape 추상 클래스의 추상 멤버인 GetArea() 메서드를 구현
     getArea(): number {
         return this.size ** 2; 
     }
@@ -42,5 +49,3 @@ console.log(rectangle.getArea()); // 200
 const shape: Shape = new Square(10);
 const area = shape.getArea();
 console.log(area); // 100
-
-
