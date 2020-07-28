58d2ecf84c9a5cbdaf73590051a03b4d84c1ae10
RedPlus <redplus@redplus.net>
2020-07-28 14:52:23 +0900

    AbstractClass.ts

---
+++ TypeScript/19_Function/18_ArrowFunction/ArrowFunction.js
@@ -0,0 +1,11 @@
+var f1 = function (message) {
+    console.log(message);
+};
+var f2 = function (message) {
+    console.log(message);
+};
+var f3 = function (message) { return console.log(message); };
+f1("Arrow Function");
+f2("Lambda Expression");
+f3("람다 식");
+//# sourceMappingURL=ArrowFunction.js.map
---
+++ TypeScript/51_Tuple/TupleType.js
@@ -0,0 +1,5 @@
+var hello = ["Hello", "World"];
+var pi = ["PI", 3.14];
+console.log(pi[0]); // "PI"
+console.log(pi[1]); // 3.14
+//# sourceMappingURL=TupleType.js.map
