74eb5e0d4a6954fa633b03465075438e211ffa89
RedPlus <redplus@redplus.net>
2020-07-29 17:21:35 +0900

    코드 정리

---
+++ TypeScript/08_ReadLine/PlusSign.js
@@ -0,0 +1,12 @@
+﻿//// 플러스(+) 기호로 숫자 모양의 문자열을 숫자 형식으로 변경하기
+//let num = "1234";
+//typeof num
+//'string'
+//typeof +num
+//'number'
+//num + num
+//'12341234'
+//+num + +num
+//2468
+//Number(num)
+//1234
