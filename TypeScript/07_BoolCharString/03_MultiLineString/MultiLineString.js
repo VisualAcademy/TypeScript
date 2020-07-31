8529c0a7b91b6e928a947b9385b7364d9db04ba3
RedPlus <redplus@redplus.net>
2020-08-01 04:27:01 +0900

    MultiLineString

---
+++ TypeScript/07_BoolCharString/03_MultiLineString/MultiLineString.js
@@ -0,0 +1,5 @@
+// MultiLineString.ts
+// 백틱(`) 기호를 사용하여 여러 줄 문자열 저장하기
+var multiLines = "\n    \uC548\uB155\uD558\uC138\uC694.\n    \uBC18\uAC11\uC2B5\uB2C8\uB2E4.\n";
+console.log(multiLines);
+//# sourceMappingURL=MultiLineString.js.map
---
+++ TypeScript/07_BoolCharString/03_MultiLineString/MultiLineString.ts
@@ -0,0 +1,8 @@
+﻿// MultiLineString.ts
+// 백틱(`) 기호를 사용하여 여러 줄 문자열 저장하기
+var multiLines = `
+    안녕하세요.
+    반갑습니다.
+`;
+
+console.log(multiLines);
