24adc54cb51b1bd5832cb171fc61ba64c68576af
RedPlus <redplus@redplus.net>
2020-08-01 04:53:22 +0900

    EscapeSequenceDemo

---
+++ TypeScript/07_BoolCharString/EscapeSequenceDemo/EscapeSequenceDemo.js
@@ -0,0 +1,17 @@
+// EscapeSequenceDemo.ts
+// 문자열과 이스케이스 시퀀스 함께 사용하기 
+// \n: 줄바꿈
+console.log("안녕\n하세요.");
+// \t: 탭만큼 들여쓰기
+console.log("반갑\t습니다.");
+// \': 작은따옴표 문자 하나 표현
+console.log(" '낼' \'만나요.\' ");
+// \": 큰따옴표 문자 하나 표현
+console.log(" \"어디\"에서요? ");
+// \\: 백(역)슬래시(\) 기호 자체를 표현
+console.log(" \\: 백슬래시 표현");
+// 백슬래시 다음에 나오는 문자 하나는 이스케이프 문자로 인식
+console.log(" C:\\Home\\VisualAcademy\\Default.aspx ");
+// 문자열을  ` 기호를 묶으면 이스케이프 시퀀스 포함
+console.log("\n\n    \uC548\uB155\uD558\uC138\uC694.\n    \uBC18\uAC11\uC2B5\uB2C8\uB2E4.\n        \n");
+//# sourceMappingURL=EscapeSequenceDemo.js.map
