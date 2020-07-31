d54e12d83ad0c0d24caf48c64c854e33785a4c0d
RedPlus <redplus@redplus.net>
2020-08-01 05:02:58 +0900

    StringInterpolationDescription

---
+++ TypeScript/07_BoolCharString/04_StringInterpolationDescription/StringInterpolationDescription.js
@@ -0,0 +1,5 @@
+// 템플릿 문자열(문자열 보간법, 보간된 문자열)
+var num = 3;
+var result = "홀수";
+console.log(num + "\uC740(\uB294) " + result + "\uC785\uB2C8\uB2E4.");
+//# sourceMappingURL=StringInterpolationDescription.js.map
---
+++ TypeScript/07_BoolCharString/04_StringInterpolationDescription/StringInterpolationDescription.ts
@@ -0,0 +1,4 @@
+﻿// 템플릿 문자열(문자열 보간법, 보간된 문자열)
+let num = 3;
+let result = "홀수";
+console.log(`${num}은(는) ${result}입니다.`);
