7ce7e3a0aa64ee5a9d0be75a39901055a7361b06
RedPlus <redplus@redplus.net>
2020-08-01 06:47:02 +0900

    Immutable

---
+++ TypeScript/07_BoolCharString/08_Immutable/Immutable.js
@@ -0,0 +1,9 @@
+var Immutable;
+(function (Immutable) {
+    // 상수: 변하지 않는 값
+    var name = "RedPlus";
+    // 변수: 변하는 값
+    var age = 21;
+    console.log("Name: " + name + ", Age: " + age);
+})(Immutable || (Immutable = {}));
+//# sourceMappingURL=Immutable.js.map
---
+++ TypeScript/07_BoolCharString/08_Immutable/Immutable.ts
@@ -0,0 +1,9 @@
+﻿module Immutable {
+    // 상수: 변하지 않는 값
+    const name = "RedPlus";
+
+    // 변수: 변하는 값
+    var age = 21;
+
+    console.log(`Name: ${name}, Age: ${age}`);
+}
