21f39ceaaebe439b1978ebf2290b98a790c5139d
RedPlus <redplus@redplus.net>
2020-08-09 09:51:42 +0900

    Property

---
+++ TypeScript/38_Property/01_Property/Property.ts
@@ -0,0 +1,13 @@
+﻿//[?] 속성(Property) 만들고 사용하기
+class Developer {
+    public name!: string;
+}
+
+//[1] 클래스의 인스턴스 새성
+const developer = new Developer();
+
+//[2] 속성에 값 설정(set): 세터
+developer.name = "박용준";
+
+//[3] 속성의 값 조회(get): 게터 
+console.log(developer.name);
