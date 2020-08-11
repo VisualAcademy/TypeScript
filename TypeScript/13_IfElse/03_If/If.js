529bab0720dea898da7e2a1b4301bf6575c0cd73
RedPlus <redplus@redplus.net>
2020-08-11 17:18:00 +0900

    코드 정리

--- TypeScript/13_IfElse/03_If/If.js
+++
@@ -1,9 +0,0 @@
-var If;
-(function (If) {
-    var score = 60;
-    // score가 60 이상이면 "합격" 출력
-    if (score >= 60) {
-        console.log("합격");
-    }
-})(If || (If = {}));
-//# sourceMappingURL=If.js.map
