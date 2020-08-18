5b7efea5349092b5dcd57452819e45182e448e88
RedPlus <redplus@redplus.net>
2020-08-18 13:07:10 +0900

    ClassNote

---
+++ TypeScript/24_Class/01_ClassNote/ClassNote.js
@@ -0,0 +1,18 @@
+// ClassNote.ts: 클래스 레벨의 메서드 호출
+var ClassNote;
+(function (ClassNote) {
+    var Car = /** @class */ (function () {
+        function Car() {
+        }
+        Car.run = function () {
+            console.log("[1] Car 클래스의 정적 메서드 run 메서드 실행");
+        };
+        Car.prototype.go = function () {
+            console.log("[2] Car 클래스의 인스턴스 메서드 go 메서드 실행");
+        };
+        return Car;
+    }());
+    Car.run(); //[1] 클래스.메서드() 형태로 호출
+    var car = new Car();
+    car.go(); //[2] 개체.메서드() 형태로 호출
+})(ClassNote || (ClassNote = {}));
---
+++ TypeScript/24_Class/01_ClassNote/ClassNote.ts
@@ -0,0 +1,14 @@
+﻿// ClassNote.ts: 클래스 레벨의 메서드 호출
+namespace ClassNote {
+    class Car {
+        static run() {
+            console.log("[1] Car 클래스의 정적 메서드 run 메서드 실행");
+        }
+        go() {
+            console.log("[2] Car 클래스의 인스턴스 메서드 go 메서드 실행");
+        }
+    }
+
+    Car.run(); //[1] 클래스.메서드() 형태로 호출
+    var car = new Car(); car.go(); //[2] 개체.메서드() 형태로 호출
+}
