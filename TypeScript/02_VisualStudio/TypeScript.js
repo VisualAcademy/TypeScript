4087a15c46c0a82c96b63871fb61f786ff526efe
RedPlus <redplus@redplus.net>
2020-08-23 06:10:20 +0900

    VariableDescription

--- TypeScript/02_VisualStudio/TypeScript.js
+++ TypeScript/02_VisualStudio/TypeScript.js
@@ -1,1 +1,2 @@
 console.log("타입스크립트 개발 환경 구축");
+//# sourceMappingURL=TypeScript.js.map
---
+++ TypeScript/05_Variable/VariableDescription.ts
@@ -0,0 +1,11 @@
+﻿// 변수(Variable): 프로그램에서 사용할 데이터를 임시로 저장해 놓는 그릇
+namespace VariableDescription {
+
+    //[1] num 이름의 변수 만들고 1234로 초기화하기
+    var num = 1234; 
+
+    //[2] 변수 사용(참조)
+    console.log(num);
+    console.log(num + num);
+
+}
