07dd44c3f1032ae46c0082ff010ab9f4e81523cb
RedPlus <redplus@redplus.net>
2020-08-23 07:09:08 +0900

    StringKeyword

---
+++ TypeScript/07_BooleanString/StringKeyword.js
@@ -0,0 +1,7 @@
+var StringKeyword;
+(function (StringKeyword) {
+    var name = "박용준";
+    var age = 21;
+    console.log("안녕하세요. " + name + "(" + age + ")입니다.");
+})(StringKeyword || (StringKeyword = {}));
+//# sourceMappingURL=StringKeyword.js.map
---
+++ TypeScript/07_BooleanString/StringKeyword.ts
@@ -0,0 +1,6 @@
+﻿namespace StringKeyword {
+    const name: string = "박용준";
+    let age: number = 21;
+
+    console.log("안녕하세요. " + name + "(" + age + ")입니다.");
+}
