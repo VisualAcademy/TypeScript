c76a0c0193e478b7d849eb2361b8ea4531ed07ce
RedPlus <redplus@redplus.net>
2020-08-30 11:23:16 +0900

    LiteralType

---
+++ TypeScript/22_Struct/LiteralType.js
@@ -0,0 +1,14 @@
+// 리터럴 형식
+var LiteralType;
+(function (LiteralType) {
+    var language;
+    language = "TypeScript";
+    console.log(language);
+    var languages;
+    languages = "C#";
+    console.log(languages);
+    var version;
+    version = 4;
+    console.log(language + " " + version);
+})(LiteralType || (LiteralType = {}));
+//# sourceMappingURL=LiteralType.js.map
---
+++ TypeScript/22_Struct/LiteralType.ts
@@ -0,0 +1,14 @@
+﻿// 리터럴 형식
+namespace LiteralType {
+    let language: "TypeScript";
+    language = "TypeScript";
+    console.log(language);
+
+    let languages: "TypeScript" | "C#";
+    languages = "C#";
+    console.log(languages);
+
+    let version: 4 | 8;
+    version = 4;
+    console.log(`${language} ${version}`);
+}
