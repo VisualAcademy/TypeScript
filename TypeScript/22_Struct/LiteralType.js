3a100215c62d6aa62352a0b73ee6f97169d1916b
RedPlus <redplus@redplus.net>
2020-08-30 11:36:58 +0900

    LiteralType

--- TypeScript/22_Struct/LiteralType.js
+++ TypeScript/22_Struct/LiteralType.js
@@ -1,14 +1,15 @@
 // 리터럴 형식
 var LiteralType;
 (function (LiteralType) {
-    var language;
-    language = "TypeScript";
-    console.log(language);
-    var languages;
-    languages = "C#";
-    console.log(languages);
-    var version;
+    var ts; //[1] 문자열 리터럴 형식
+    ts = "TypeScript";
+    //ts = "C#"; // 이 코드는 에러 발생
+    console.log(ts);
+    var cs; //[2] 여러 문자열 리터럴 형식
+    cs = "C#";
+    console.log(cs);
+    var version; //[3] 숫자 리터럴 형식
     version = 4;
-    console.log(language + " " + version);
+    console.log(ts + " " + version);
 })(LiteralType || (LiteralType = {}));
 //# sourceMappingURL=LiteralType.js.map
--- TypeScript/22_Struct/LiteralType.ts
+++ TypeScript/22_Struct/LiteralType.ts
@@ -1,14 +1,15 @@
 ﻿// 리터럴 형식
 namespace LiteralType {
-    let language: "TypeScript";
-    language = "TypeScript";
-    console.log(language);
+    let ts: "TypeScript"; //[1] 문자열 리터럴 형식
+    ts = "TypeScript";
+    //ts = "C#"; // 이 코드는 에러 발생
+    console.log(ts);
 
-    let languages: "TypeScript" | "C#";
-    languages = "C#";
-    console.log(languages);
+    let cs: "TypeScript" | "C#"; //[2] 여러 문자열 리터럴 형식
+    cs = "C#";
+    console.log(cs);
 
-    let version: 4 | 8;
+    let version: 4 | 8; //[3] 숫자 리터럴 형식
     version = 4;
-    console.log(`${language} ${version}`);
+    console.log(`${ts} ${version}`);
 }
