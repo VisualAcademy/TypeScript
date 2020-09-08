910af710d84c4f4fc6ed6431b5c79690c984db51
RedPlus <redplus@redplus.net>
2020-09-09 07:19:32 +0900

    ArrayOfType

---
+++ TypeScript/28_GenericClass/ArrayOfType.js
@@ -0,0 +1,8 @@
+var ArrayOfType;
+(function (ArrayOfType) {
+    var stack = [];
+    stack.push("First"); // push() 메서드는 문자열만 입력 가능
+    stack.push("Second");
+    console.log(stack.pop()); // 문자열로 바로 출력
+    console.log(stack.pop());
+})(ArrayOfType || (ArrayOfType = {}));
---
+++ TypeScript/28_GenericClass/ArrayOfType.ts
@@ -0,0 +1,7 @@
+﻿namespace ArrayOfType {
+    let stack: Array<string> = [];
+    stack.push("First"); // push() 메서드는 문자열만 입력 가능
+    stack.push("Second");
+    console.log(stack.pop()); // 문자열로 바로 출력
+    console.log(stack.pop());
+}
