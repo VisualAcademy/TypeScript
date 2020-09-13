6225006f5dd90480e74f2753f5eda33e55719683
RedPlus <redplus@redplus.net>
2020-09-13 09:54:01 +0900

    Terms

---
+++ TypeScript/07_BooleanString/Terms.ts
@@ -0,0 +1,7 @@
+﻿namespace Terms {
+    //[?] 객체(Object), 개체(Object), 오브젝트, 클래스(Class), 인스턴스
+    //[!] Date 객체/클래스의 인스턴스 생성
+    const dateNow = new Date(); // Date 클래스의 생성자를 사용하여 dateNow 개체 생성
+    console.log(dateNow);
+    //[*] Date 객체, Date 개체, Date 오브젝트, Date 클래스, Date 인스턴스, Date 생성자
+}
