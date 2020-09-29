6e66a8a50663646136851c05bc5f3e49373fb22d
RedPlus <redplus@redplus.net>
2020-09-29 15:40:39 +0900

    ES6_TypeScriptImpliedType

---
+++ TypeScript/18_Array/ES6_TypeScriptImpliedType.ts
@@ -0,0 +1,18 @@
+﻿// TypeScriptImpliedType.txt
+// Implied Type
+
+var arr = [1, 2, 3];
+
+// var a = arr[0];
+// var b = arr[1];
+// var c = arr[2];
+var [a, b, c] = arr;
+
+document.write(a + "<br />");
+document.write(b + "<br />");
+document.write(c + "<br />");
+
+var [d, e, f] = ["string", 1234, true];
+document.write(d + "<br />");
+document.write(e + "<br />");
+document.write(f + "<br />");
